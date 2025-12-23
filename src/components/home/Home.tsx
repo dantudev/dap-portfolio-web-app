fa20f7844976502453a0718ea83b0cf33a39bdf4
David Antunez Perez <A203475@dekra.org>
2025-12-23 13:44:50 +0100

    feat: added basic content and header, displaying title and subtitle

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <section className='flex w-full flex-1 flex-col items-center justify-center gap-6'>
+      <div className='flex flex-col items-center justify-center'>
+        <h1 className='text-[64px] font-black'>David Antúnez Pérez</h1>
+        <h2 className='text-[48px] font-light'>
+          Frontend Developer & UX/UI Designer
+        </h2>
+      </div>
+    </section>
+  )
+}
+
+export default Home
