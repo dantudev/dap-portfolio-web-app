ee23d6dc4ad1ed7c83eb62643cd7118a918e37f0
David Antunez Perez <A203475@dekra.org>
2025-12-23 12:16:19 +0100

    build!: added tailwind custom config and heroui dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@heroui/react"
+
 function App() {
   return (
     <div className='w-full h-screen flex flex-col items-center justify-center'>
       Hello, World
+      <Button color='primary'>Button</Button>
     </div>
   )
 }
