84a9177d75242a51ec6e5369729a1e3454130d72
David Antunez Perez <A203475@dekra.org>
2025-12-23 13:20:13 +0100

    build: added prettier-tailwind plugin and font inter variable

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,21 @@
-import {heroui} from "@heroui/theme"
+import { heroui } from '@heroui/theme'
 
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
+    './index.html',
     './src/layouts/**/*.{js,ts,jsx,tsx,mdx}',
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
+    './node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: [['Inter Variable', 'Inter', 'ui-sans-serif', 'system-ui']],
+      },
+    },
   },
-  darkMode: "class",
+  darkMode: 'class',
   plugins: [heroui()],
 }
