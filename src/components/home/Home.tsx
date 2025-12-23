9642fac4bd7efda2bc6ff77bb413b0ca78a5396e
David Antunez Perez <A203475@dekra.org>
2025-12-23 13:53:47 +0100

    feat: added custom avatar

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,7 +1,15 @@
+import { Avatar } from '@heroui/avatar'
+
 function Home() {
   return (
     <section className='flex w-full flex-1 flex-col items-center justify-center gap-6'>
       <div className='flex flex-col items-center justify-center'>
+        <Avatar
+          alt='David Antúnez Pérez'
+          className='h-60 w-60'
+          isBordered
+          src='/public/images/david_avatar.webp'
+        />
         <h1 className='text-[64px] font-black'>David Antúnez Pérez</h1>
         <h2 className='text-[48px] font-light'>
           Frontend Developer & UX/UI Designer
