e85f4383303d376286c350df70b30eb7f7cbfd20
David Antunez Perez <A203475@dekra.org>
2025-12-26 11:17:48 +0100

    feat: added scroll down icon and url links

---
+++ src/components/home/scroll-down-indicator/ScrollDownIndicator.tsx
@@ -0,0 +1,26 @@
+import styles from './ScrollDownIndicator.module.css'
+import { Tooltip } from '@heroui/tooltip'
+
+function ScrollDownIndicator() {
+  const handleClick = () => {
+    const target = document.getElementById('section-2')
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' })
+    }
+  }
+
+  return (
+    <Tooltip
+      content='Scroll Down'
+      showArrow={true}
+    >
+      <div
+        className={styles['mouse-icon'] + ' cursor-pointer'}
+        onClick={handleClick}
+      >
+        <div className={styles['scroll-wheel']} />
+      </div>
+    </Tooltip>
+  )
+}
+export default ScrollDownIndicator
