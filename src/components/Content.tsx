da4e4e7a06510fc5b7b12ee3d5657862551c2a6b
David Antúnez Pérez <91873698+DavidAntunezPerez@users.noreply.github.com>
2025-12-29 10:57:33 +0100

    refactor(components): avoiding double source of truth when calling navigation menu items in components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,21 +1,14 @@
+import { MENU_ITEMS } from '@app/consts/navigation.consts'
 import Home from './home/Home'
 
 function Content() {
-  const sections = [
-    { id: 'home', label: 'Home', bg: 'bg-white' },
-    { id: 'experience', label: 'Experience', bg: 'bg-blue-100' },
-    { id: 'projects', label: 'Projects', bg: 'bg-green-100' },
-    { id: 'stack', label: 'Stack', bg: 'bg-yellow-100' },
-    { id: 'contact', label: 'Contact', bg: 'bg-pink-100' },
-  ]
-
   return (
     <main className='flex w-full flex-1 flex-col'>
-      {sections.map((section) => (
+      {MENU_ITEMS.map((section) => (
         <section
           key={section.id}
           id={section.id}
-          className={`${section.bg} flex w-full items-center justify-center min-h-dvh`}
+          className={`${section.bg} flex min-h-dvh w-full items-center justify-center`}
         >
           {section.id === 'home' ? (
             <Home />
