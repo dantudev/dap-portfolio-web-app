425d07eaa8d1ebd0f91de113f72839d11e587377
David Antúnez Pérez <91873698+DavidAntunezPerez@users.noreply.github.com>
2026-01-19 16:47:00 +0100

    feat(components)!: experience section first version and components

---
+++ src/app/consts/content/experience.consts.ts
@@ -0,0 +1,107 @@
+import type { Experiences } from 'src/types/experiences.types'
+
+export const workExperiences: Experiences[] = [
+  {
+    logo: '/images/dekra_logo.webp',
+    title: 'Frontend Developer & UX/UI Designer',
+    company: 'DEKRA Digital & Product Solutions',
+    period: 'August 2023 - Present',
+    description:
+      'Design and development of web applications supporting AI solutions (e.g., chatbots, image recognition tools, internal dashboards). Bridging frontend development with UX/UI design responsibilities, while collaborating with international teams across different countries using English as the main working language.',
+    technologies: [
+      'React',
+      'Angular',
+      'Figma',
+      'TypeScript',
+      'Tailwind CSS',
+      'Excalidraw',
+      'Git',
+      'HTML',
+      'CSS',
+      'Azure DevOps',
+      'Jira',
+      'React Native',
+      'Java',
+      'Kotlin',
+      'Python',
+      'Spring Boot',
+      'Microsoft Azure',
+    ],
+    location: 'Málaga, Spain',
+  },
+  {
+    title: 'Software Developer',
+    period: 'January 2023 - June 2023',
+    description:
+      'Developed and maintained multiple client projects, implemented responsive designs, and integrated third-party APIs for enhanced functionality.',
+    technologies: ['React', 'Express.js', 'PostgreSQL', 'Docker', 'Redis'],
+    location: 'Stuttgart, Germany & Málaga, Spain',
+  },
+  {
+    title: 'AI Intern',
+    period: 'March 2022 - June 2022',
+    description:
+      'Created responsive and interactive user interfaces, collaborated with designers, and optimized application performance.',
+    technologies: ['React', 'JavaScript', 'SASS', 'Webpack', 'Jest'],
+    location: 'Málaga, Spain',
+  },
+]
+
+export const educationExperiences: Experiences[] = [
+  {
+    logo: '/images/zaidin.png',
+    title: 'Curso de Especialización, Inteligencia Artificial & Big Data',
+    company: 'IES Zaidín-Vergeles',
+    period: 'August 2023 - Present',
+    description:
+      'Design and development of web applications supporting AI solutions (e.g., chatbots, image recognition tools, internal dashboards). Bridging frontend development with UX/UI design responsibilities, while collaborating with international teams across different countries using English as the main working language.',
+    technologies: [
+      'React',
+      'Angular',
+      'Figma',
+      'TypeScript',
+      'Tailwind CSS',
+      'Excalidraw',
+      'Git',
+      'HTML',
+      'CSS',
+      'Azure DevOps',
+      'Jira',
+      'React Native',
+      'Java',
+      'Kotlin',
+      'Python',
+      'Spring Boot',
+      'Microsoft Azure',
+    ],
+    location: 'Málaga, Spain',
+  },
+  {
+    logo: '/images/cpifpalanturing_logo.webp',
+    title: 'CFGS, Desarrollo de Aplicaciones Multiplataforma',
+    company: 'Centro Público Integrado de FP Alan Turing',
+    period: 'January 2023 - June 2023',
+    description:
+      'Developed and maintained multiple client projects, implemented responsive designs, and integrated third-party APIs for enhanced functionality.',
+    technologies: ['React', 'Express.js', 'PostgreSQL', 'Docker', 'Redis'],
+    location: 'Stuttgart, Germany & Málaga, Spain',
+  },
+  {
+    logo: '/images/iesmartinaldehuela.webp',
+    title: 'Bachillerato, Tecnología de la ingeniería',
+    company: 'IES Martín Aldehuela',
+    period: 'March 2022 - June 2022',
+    description:
+      'Created responsive and interactive user interfaces, collaborated with designers, and optimized application performance.',
+    technologies: ['React', 'JavaScript', 'SASS', 'Webpack', 'Jest'],
+    location: 'Málaga, Spain',
+  },
+  {
+    title: 'Educación Secundaria Obligatoria (ESO), Científico-tecnológico',
+    period: 'March 2022 - June 2022',
+    description:
+      'Created responsive and interactive user interfaces, collaborated with designers, and optimized application performance.',
+    technologies: ['React', 'JavaScript', 'SASS', 'Webpack', 'Jest'],
+    location: 'Málaga, Spain',
+  },
+]
---
+++ src/types/experiences.types.ts
@@ -0,0 +1,19 @@
+interface Experiences {
+  company?: string
+  description: string
+  period: string
+  technologies: string[]
+  title: string
+  logo?: string
+  location?: string
+}
+
+const ExperienceTabEnum = {
+  WORK: 'work',
+  EDUCATION: 'education',
+} as const
+
+type ExperienceTab = (typeof ExperienceTabEnum)[keyof typeof ExperienceTabEnum]
+
+export { ExperienceTabEnum }
+export type { Experiences, ExperienceTab }
